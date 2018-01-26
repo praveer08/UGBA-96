3a1670adcb9fc1b56bb84ff549544f5002f5bf86
robertSweeney <robertsweeneyblanco@gmail.com>
2018-01-25 17:54:09 -0800

    changed q1 test

--- Pset 1/tests/q1.py
+++ Pset 1/tests/q1.py
@@ -1,2 +1,20 @@
 test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""                         
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
 }
