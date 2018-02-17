b9c2b47ea965e7ec8ebcf8314b23658056884a7a
conrad-miller <35392137+conrad-miller@users.noreply.github.com>
2018-02-16 18:43:33 -0800

    Add files via upload

---
+++ pset2/tests/q1.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> x                          # change this line
+          91                             # change this line
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
